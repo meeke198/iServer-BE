b86bd802aa046960e77252bba864805e53b7ca2a
hien nguyen <thuhiennguyen198@gmail.com>
2023-05-17 09:38:23 -0400

    comment out table

--- IServer-BE/src/main/java/com/IServerBE/entity/Meal.java
+++ IServer-BE/src/main/java/com/IServerBE/entity/Meal.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="meal")
+//@Table(name="meal")
 public class Meal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
