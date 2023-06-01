3d2c9b36fe5004e2620b485af08d013c77fe9eb6
hien nguyen <thuhiennguyen198@gmail.com>
2023-06-01 02:15:06 -0400

    add database sql statements

--- IServer-BE/src/main/resources/database/abc.sql
+++ IServer-BE/src/main/resources/database/abc.sql
@@ -1,14 +1,14 @@
 use `iserver-v1`;
-INSERT INTO user (id, create_at, username, password, email, avatar, is_active, remember_token)
-VALUES (1, '2023-05-26 10:30:00', 'john_doe', 'abc123', 'john@example.com', 'avatar.jpg', 1, 'XYZ123');
-INSERT INTO user (id, create_at, username, password, email, avatar, is_active, remember_token)
-VALUES (2, '2023-05-26 11:15:00', 'jane_smith', 'password456', 'jane@example.com', 'user.png', 1, NULL);
-INSERT INTO user (id, create_at, username, password, email, avatar, is_active, remember_token)
-VALUES (3, '2023-05-26 12:00:00', 'michael_johnson', 'pass789', 'michael@example.com', 'default.jpg', 0, 'ABC789');
-INSERT INTO user (id, create_at, username, password, email, avatar, is_active, remember_token)
-VALUES (4, '2023-05-26 13:30:00', 'hiennguyen', 'qwerty', 'longusername@example.com', 'user.jpg', 1, 'DEF321');
-INSERT INTO user (id, create_at, username, password, email, avatar, is_active, remember_token)
-VALUES (5, '2023-05-26 14:45:00', 'emma_smith', 'password123', 'emma@example.com', 'user2.jpg', 1, NULL);
+INSERT INTO user (id, create_at, username, password, email, avatar, is_active, is_online)
+VALUES (1, '2023-05-26 10:30:00', 'john_doe', 'abc123', 'john@example.com', 'avatar.jpg', 1, 1);
+INSERT INTO user (id, create_at, username, password, email, avatar, is_active, is_online)
+VALUES (2, '2023-05-26 11:15:00', 'jane_smith', 'password456', 'jane@example.com', 'user.png', 1, 1);
+INSERT INTO user (id, create_at, username, password, email, avatar, is_active, is_online)
+VALUES (3, '2023-05-26 12:00:00', 'michael_johnson', 'pass789', 'michael@example.com', 'default.jpg', 0, 1);
+INSERT INTO user (id, create_at, username, password, email, avatar, is_active, is_online)
+VALUES (4, '2023-05-26 13:30:00', 'hiennguyen', 'qwerty', 'longusername@example.com', 'user.jpg', 1, 1);
+INSERT INTO user (id, create_at, username, password, email, avatar, is_active, is_online)
+VALUES (5, '2023-05-26 14:45:00', 'emma_smith', 'password123', 'emma@example.com', 'user2.jpg', 1, 1);
 select * from user;
 INSERT INTO room (id, name, create_at)
 VALUES (1, 'Meeting Room 1', '2023-05-26 10:30:00');
@@ -43,5 +43,6 @@ VALUES (4, 'Goodbye!', 3, 1, '2023-05-26 13:30:00');
 INSERT INTO message (id, content, user_id, room_id, sent_at)
 VALUES (5, 'How are you?', 2, 2, '2023-05-26 13:30:00');
 select * from message;
+
 
 
